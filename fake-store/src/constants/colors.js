b545bd6a04bb736bfc521ffa1c55025715a234e5
fafadu <104352818+fafadu@users.noreply.github.com>
2024-04-26 15:40:08 +1000

    Add new color constants and update Categories screen

---
+++ fake-store/src/constants/colors.js
@@ -0,0 +1,11 @@
+const colors = {
+    white: '#FFFFFF',
+    lightBlue: '#ADD8E6',
+    darkBlue: '#0000FF',
+    grey: '#dedede',
+    black: '#000000',
+    // 添加其他颜色...
+  };
+  
+  export default colors;
+  
--- fake-store/src/screens/Categories.js
+++ fake-store/src/screens/Categories.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import colors from '../constants/colors';
+
+const categories = [
+  { id: '1', title: 'Electronics' },
+  { id: '2', title: 'Jewelery' },
+  { id: '3', title: 'Men\'s Clothing' },
+  { id: '4', title: 'Women\'s Clothing' },
+];
+
+
 
 export const Categories =()=>{
+  
+  const navigation = useNavigation();
+
+
+  const renderCategoryItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => {
+        // Handle the press
+        console.log('Pressed', item.title);
+        // Navigate to the category's product list
+        // navigation.navigate('ProductList', { category: item.title });
+      }}
+    >
+      <Text style={styles.title}>{item.title}</Text>
+    </TouchableOpacity>
+  );
   
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Categories</Text>
+      <FlatList
+        data={categories}
+        renderItem={renderCategoryItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.listContainer}
+      />
     </View>
   );
 };
@@ -16,27 +49,31 @@ export const Categories =()=>{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: 'white',
+    marginTop: 50, // Adjust the margin as needed
+    backgroundColor: colors.white,
   },
   header: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 20,
+    textAlign: 'center', // Center the header text
+  },
+  listContainer: {
+    alignItems: 'stretch', // Stretch list items to match the container width
   },
   item: {
-    backgroundColor: 'lightblue',
+    backgroundColor: colors.lightBlue,
     padding: 20,
     marginVertical: 8,
+    marginHorizontal: 16, // Adjust horizontal margin to match your design
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: '#dedede',
+    borderColor: colors.grey,
   },
   title: {
     fontSize: 18,
   },
 });
 
-// export default CategoriesScreen;
