c2b49987fb0bdd241c12ea76a446743ac9c81f1a
fafadu <104352818+fafadu@users.noreply.github.com>
2024-04-26 15:50:05 +1000

    update Categories.js API

--- fake-store/src/screens/Categories.js
+++ fake-store/src/screens/Categories.js
@@ -1,46 +1,57 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList,ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import colors from '../constants/colors';
 
-const categories = [
-  { id: '1', title: 'Electronics' },
-  { id: '2', title: 'Jewelery' },
-  { id: '3', title: 'Men\'s Clothing' },
-  { id: '4', title: 'Women\'s Clothing' },
-];
 
+export const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch('https://fakestoreapi.com/products/categories');
+        const data = await response.json();
+        console.log(data)
+        setCategories(data.map((category) => ({ id: category, title: category })));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-export const Categories =()=>{
-  
-  const navigation = useNavigation();
-
+    fetchCategories();
+  }, []);
 
   const renderCategoryItem = ({ item }) => (
     <TouchableOpacity
       style={styles.item}
       onPress={() => {
-        // Handle the press
+        // 此处处理点击事件，比如导航到相应分类的产品列表
         console.log('Pressed', item.title);
-        // Navigate to the category's product list
         // navigation.navigate('ProductList', { category: item.title });
       }}
     >
       <Text style={styles.title}>{item.title}</Text>
     </TouchableOpacity>
   );
-  
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={categories}
-        renderItem={renderCategoryItem}
-        keyExtractor={(item) => item.id}
-        contentContainerStyle={styles.listContainer}
-      />
+      {loading ? (
+        <ActivityIndicator size="large" color={colors.darkBlue} />
+      ) : (
+        <FlatList
+          data={categories}
+          renderItem={renderCategoryItem}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.listContainer}
+        />
+      )}
     </View>
   );
 };
