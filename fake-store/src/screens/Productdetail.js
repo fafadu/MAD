6371994fd438505ef318f44232195bb34c4f464f
fafadu <104352818+fafadu@users.noreply.github.com>
2024-04-27 16:17:52 +1000

    add Imgbutton.js, TButton.js

--- fake-store/src/screens/Productdetail.js
+++ fake-store/src/screens/Productdetail.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { useRoute } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
-import { View, Text, TouchableOpacity, StyleSheet, FlatList,ActivityIndicator,Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList,ActivityIndicator,Image,ScrollView } from 'react-native';
 import colors from '../constants/colors';
+import { ImgButton } from '../coponents/ImgButton';
+import { TButton } from '../coponents/TButton';
 
 
 export const Productdetail = () => {
@@ -39,9 +41,14 @@ export const Productdetail = () => {
       <View>
         <Text style={styles.title}>{product.title}</Text>
         <Text>Price: ${product.price}</Text>
-        <Text>Description: {product.description}</Text>
-        <Image source={{ uri: product.image }} style={styles.image} />
         
+        <Image source={{ uri: product.image }} style={styles.image} />
+        <View style={styles.descriptionContainer}>
+        <ScrollView>
+          <Text style={styles.description}>{product.description}</Text>
+        </ScrollView>
+        </View>
+        <TButton title="Back" back={true}/>
       </View>
     )}
       </View>
@@ -83,6 +90,21 @@ export const Productdetail = () => {
         height: 200, // 根据你的布局需求调整
         resizeMode: 'contain',
         marginBottom: 10,
+      },
+      descriptionContainer: {
+        flex: 1,  // 使得容器占满剩余空间
+        minHeight: 150, // 这将确保容器至少有100单位的高度
+        padding: 5,
+        // marginVertical: 10,  // 在描述上下添加空间
+        marginHorizontal:10,
+        borderWidth: 1,
+        borderColor: colors.black,
+        borderRadius: 0,
+        backgroundColor: colors.lightBlue,  // 可以根据你的设计进行修改
+      },
+      description: {
+        fontSize: 16,
+        color: colors.black,  // 颜色可以根据需要调整
       },
   });
   
